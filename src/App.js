ef6e6eacc8cd5cc431f116f40990bfad034b23da
anewmodern <jimmleb178@gmail.com>
2019-01-09 16:20:47 -0800

    added 4squared api call

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import GoogleMaps from './Components/GoogleMaps';
 import Geolocation from './Components/Geolocation';
+import axios from 'axios';
 
 class App extends Component {
   state = {
     lat: 0,
     lng: 0,
-    geolocationArrived: false
+    geolocationArrived: false,
+    venues: []
   }
   setGeolocation = (lat, lng) => {
     console.log(lat, lng);
@@ -17,13 +19,33 @@ class App extends Component {
         lng,
         geolocationArrived: true
       };
+    }, () => {
+      this.getVenues();
     });
+  }
+  getVenues = () => {
+    const url = 'https://api.foursquare.com/v2/venues/explore?';
+    const parameters = {
+      client_id: 'QP5MCIB1BHHWKNDS5PDWMIAP535T3MDWACJKHPTTY52WHZ0S',
+      client_secret: 'ABW2QVCLNJXGCHE3YJ0NHG4UVEQJK5N1SKS3L3TFYAATKPNC',
+      query: 'food',
+      ll: `${this.state.lat}, ${this.state.lng}`,
+      v: '20190109'
+    }
+
+    axios.get(url + new URLSearchParams(parameters)).then((response) => {
+      const venues = response.data.response.groups[0].items;
+      console.log(venues);
+      this.setState(() => ({ venues }));
+    }).catch((error) => {
+      console.log('Error, ' + error);
+    })
   }
   render() {
     return (
       <div className="App">
         <Geolocation setGeolocation={this.setGeolocation} />
-        {this.state.geolocationArrived && <GoogleMaps location={{lat: this.state.lat, lng: this.state.lng}} />}
+        {this.state.geolocationArrived && <GoogleMaps location={{lat: this.state.lat, lng: this.state.lng}} venues={this.state.venues} />}
       </div>
     );
   }
